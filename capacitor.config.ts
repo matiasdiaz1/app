447a4ced2d93247ca9ca4448c71588e71ad56674
Mati <mat.diaza@duocuc.cl>
2024-11-29 19:35:16 -0300

    se agrego icono

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,26 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'app',
-  webDir: 'www'
+  appId: 'com.myapp.registrapp',
+  appName: 'RegistrApp',
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#dde7f0",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: false,
+      splashImmersive: false,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
